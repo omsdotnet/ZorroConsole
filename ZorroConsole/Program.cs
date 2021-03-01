165cf930a78144127c50078af59430b03223bb13
Pavel <kulbida@hotmail.com>
2021-03-01 23:09:21 +0600

    add true zerroconsole

---
+++ ZorroConsole/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace ZorroConsole
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      bool repeat;
+      string userInput = "APPTEST START";
+      const string stopWord = "APPTEST STOP";
+
+      string serviceUrl = !string.IsNullOrEmpty(args.FirstOrDefault()) ? args.First() :
+#if DEBUG
+      "https://localhost:44370/appconsole/";
+#else
+     "https://zorrobackend.azurewebsites.net/appconsole/";
+#endif
+
+      var client = new ServiceClient(serviceUrl);
+
+      do
+      {
+        string response = client.Send(userInput);
+        
+        Console.WriteLine(response);
+
+        repeat = userInput != stopWord;
+
+        if (repeat)
+        {
+          userInput = Console.ReadLine();
+        }
+      } 
+      while (repeat);
+    }
+  }
+}
---
+++ ZorroConsole/ServiceClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZorroConsole
+{
+  internal class ServiceClient
+  {
+    private string url;
+    private static readonly HttpClient Client = new HttpClient();
+
+    public ServiceClient(string url)
+    {
+      this.url = url;
+    }
+
+    public string Send(string userInput)
+    {
+      using (HttpContent httpContent = new StringContent($"\"{userInput}\"", Encoding.UTF8, "application/json"))
+      {
+        var result = Task.Run(() => Client.PostAsync(url, httpContent)).Result;
+
+        result.EnsureSuccessStatusCode();
+
+        var resultStr = Task.Run(() => result.Content.ReadAsStringAsync()).Result;
+
+        return resultStr;
+      }
+    }
+  }
+}
